736eae7b34b0b93a98bd670753d6e18da1b501e1
Dmitriy <dima.pozdneev@yandex.ru>
2024-05-15 12:46:20 +0300

    Добавление логов

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 	"strconv"
 	"strings"
@@ -47,45 +48,57 @@ func (r *RingIntBuffer) Get() []int {
 }
 
 func read(nextStage chan<- int, done chan bool) {
+	log.Println("Запуск функции чтения из консоли")
 	scanner := bufio.NewScanner(os.Stdin)
 	var data string
 	for scanner.Scan() {
 		data = scanner.Text()
 		if strings.EqualFold(data, "exit") {
+			log.Println("Введена команда exit, выход из приложения, закрытие канала done")
 			fmt.Println("Программа завершена. ")
 			close(done)
 			return
 		}
 		i, err := strconv.Atoi(data)
 		if err != nil {
+			log.Println("Получено не целое число: ", data)
 			fmt.Println("Программа обрабатывает только целые числа")
 			continue
 		}
+		log.Println("Получено целое число: ", i)
 		nextStage <- i
 	}
 }
 
 func negativeFilterStageInt(previosStageChannel <-chan int, nextStageChannel chan<- int, done <-chan bool) {
+	log.Println("Запущена функция фильтрации отрицательных чисел.")
 	for {
 		select {
 		case data := <-previosStageChannel:
+			log.Println("Функция фильтрации отрицательных чисел, получено из канала: ", data)
 			if data > 0 {
+				log.Println("Функция фильтрации отрицательных чисел пройдена, положительное число отправлено в следующий канал: ", data)
 				nextStageChannel <- data
 			}
 		case <-done:
+			log.Println("Канал done закрыт, выход из функции фильтрации отрицательных чисел")
 			return
 		}
 	}
 }
 
 func notDividedThreeFunc(previosStageChannel <-chan int, nextStageChannel chan<- int, done <-chan bool) {
+	log.Println("Функция фильтрации деления на 3 запущена")
 	for {
 		select {
 		case data := <-previosStageChannel:
+			log.Println("Функция фильтрации деления на 3, получено из канала: ", data)
 			if data%3 == 0 {
+				log.Println("Функция фильтрации деления на 3 пройдена, число отправлено в следующий канал: ", data)
 				nextStageChannel <- data
 			}
 		case <-done:
+			log.Println("Канал done закрыт, выход из функции фильтрации деления на 3")
 			return
 		}
 	}
@@ -96,40 +109,50 @@ func bufferStageFunc(previosStageChannel <-chan int, nextStageChannel chan<- int
 	for {
 		select {
 		case data := <-previosStageChannel:
+			log.Println("Запись значения в кольцевой буфер:", data)
 			buffer.Push(data)
 		case <-time.After(interval):
 			bufferData := buffer.Get()
 			if bufferData != nil {
+				log.Println("Запись в канал из кольцового буфера по истечении", interval)
 				for _, data := range bufferData {
 					nextStageChannel <- data
 				}
 			}
 		case <-done:
+			log.Println("Канал done закрыт, выход из функции, для работы с кольцевым буфером")
 			return
 		}
 	}
 }
 func main() {
+	log.Println("Запуск приложения")
 	input := make(chan int)
 	done := make(chan bool)
+	log.Println("Создание горутины для чтения из консоли")
 	go read(input, done)
 
 	negativeFilterChannel := make(chan int)
+	log.Println("Создание горутины для фильтрации отрицательных чисел")
 	go negativeFilterStageInt(input, negativeFilterChannel, done)
 
 	notDividedThreeChannel := make(chan int)
+	log.Println("Создание горутины для фильтрации деления на 3")
 	go notDividedThreeFunc(negativeFilterChannel, notDividedThreeChannel, done)
 
 	bufferedIntChannel := make(chan int)
 	bufferSize := 10
 	bufferDrainInterval := time.Second * 30
+	log.Println("Создание горутины для работы кольцевого буфера")
 	go bufferStageFunc(notDividedThreeChannel, bufferedIntChannel, done, bufferSize, bufferDrainInterval)
 
 	for {
 		select {
 		case data := <-bufferedIntChannel:
+			log.Println("Получение из канала кольцвого буфера обработанных данных:", data)
 			fmt.Println("Обработанные данные: ", data)
 		case <-done:
+			log.Println("Канал done закрыт, завершение программы")
 			return
 		}
 	}
